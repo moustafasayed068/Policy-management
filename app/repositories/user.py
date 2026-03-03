571416d8d7e575f4bab50ccb67af03a768ae7f5a
moustafasayed068 <moustafasayed068@gmail.com>
2026-03-03 21:53:00 +0000

    Update user.py

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -5,21 +5,17 @@ from app.schemas.user import UserCreate
 from app.core.security import get_password_hash
 
 class UserRepository:
-    def __init__(self, db: AsyncSession): # غيرنا النوع لـ AsyncSession
+    def __init__(self, db: AsyncSession): 
         self.db = db
 
-    # دالة البحث - حولناها Async واستخدمنا select
     async def get_by_email(self, email: str):
         query = select(User).where(User.email == email)
         result = await self.db.execute(query)
         return result.scalars().first()
 
-    # دالة الإنشاء - حولناها Async وضفنا await للـ commit
     async def create(self, obj_in: UserCreate) -> User:
-        # 1. تشفير الباسورد
         hashed_password = get_password_hash(obj_in.password)
         
-        # 2. تحويل البيانات لـ Model
         db_obj = User(
             email=obj_in.email,
             password_hash=hashed_password,
@@ -27,7 +23,6 @@ class UserRepository:
             role=obj_in.role
         )
         
-        # 3. الحفظ (كل الخطوات لازم await)
         self.db.add(db_obj)
         await self.db.commit()
         await self.db.refresh(db_obj)
