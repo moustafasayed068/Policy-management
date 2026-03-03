72e52c76ff61acf92dfe90d919e0456c1744b4c7
moustafasayed068 <moustafasayed068@gmail.com>
2026-03-03 21:54:33 +0000

    Remove commented lines for cleaner code

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI
-from app.core.config import settings # تأكد إن المسار صح حسب اسم الفولدر عندك
-from app.api.routers import health, auth # ضفنا auth هنا
-
+from app.core.config import settings 
+from app.api.routers import health, auth 
 app = FastAPI(title="Policy Management AI")
 
-# تسجيل الروترز
 app.include_router(health.router)
-app.include_router(auth.router) # ده السطر اللي هيفتح لك صفحة الـ Login
+app.include_router(auth.router) 
 
 @app.get("/")
 async def root():
